55fb95cc4aaf01964e3cad34abb0a1a6fda04930
Meric Cirem <emericbas@gmail.com>
2023-01-04 11:54:22 +0000

    HConcreteGrandChild

---
+++ src/days/day29_Final_AbstractClasses/GAbstractChild.java
@@ -0,0 +1,21 @@
+package days.day29_Final_AbstractClasses;
+
+public abstract class GAbstractChild extends DParent{
+     /*
+        Abstract bir class'in abstract bir child class'i oldugunda
+        parent class'daki method'lari IMPLEMENT etmek ZORUNDA DEGILDIR
+     */
+
+
+
+    public void method1(){
+
+    }
+
+    public abstract void method4();
+
+
+    public void method5(){
+
+    }
+}
---
+++ src/days/day29_Final_AbstractClasses/HConcreteGrandChild.java
@@ -0,0 +1,15 @@
+package days.day29_Final_AbstractClasses;
+
+public class HConcreteGrandChild extends GAbstractChild{
+    @Override
+    public void method2() {
+
+    }
+
+    @Override
+    public void method4() {
+
+    }
+
+    }
+
